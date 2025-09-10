2be6136613314fa68e70acd774acde8c2e270d89
kim jae eun <rlawodms95@naver.com>
2025-09-10 23:54:35 +0900

    gsap적용시 푸터 안나오는 이슈 수정

--- script/main.js
+++ script/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     let header; // 헤더
+    let footer; // 푸터
     let openBg; // 인트로 애니메이션 화면
     let gnb; // GNB 네비게이션
     let gnbMenu; // GNB 네비게이션 메뉴
@@ -18,57 +19,58 @@ document.addEventListener('DOMContentLoaded', function(){
     init();
 
     function init(){
+        reloadScrollTop();
         mouseWheelEvent();
-        openBgDelete();
+        // openBgDelete();
+    }
+
+    function reloadScrollTop(){
+        setTimeout(() => {
+            window.scrollTo({top:0});
+        }, 100);
     }
 
     function mouseWheelEvent(){
         container = document.querySelector('.main-container');
         sections = document.querySelectorAll('section');
-        sectionCount = sections.length;
+        footer = document.querySelector('footer');
+        scrollTargets = [...sections, footer];
+        targetCount = scrollTargets.length;
         scrollThreadHold = 300;
         wheelCount = 0;
 
-        container.addEventListener('wheel', (e) => {
-            e.preventDefault();
+        gsap.registerPlugin(Observer, scrollTo);
 
-            if(e.deltaY > 0){
-                wheelCount += Math.abs(e.deltaY);
-            }
-            else{
-                wheelCount -= Math.abs(e.deltaY);
-            }
-
-            if(Math.abs(wheelCount) > sectionCount){
-                if(wheelCount > 0){
-                    gotoSection(contentCount + 1);
-                }
-                else{
-                    gotoSection(contentCount - 1);
-                }
-                wheelCount = 0;
-            }
+        Observer.create({
+            target: window,
+            type: 'wheel',
+            onUp: () => gotoSection(contentCount - 1),
+            onDown: () => gotoSection(contentCount + 1),
+            tolerance: 500,
+            preventDefault: true
         });
     };
-
     function gotoSection(target){
-        target = Math.max(0,Math.min(target, sectionCount - 1));
-        contentCount = target;
-        animateSection();
-    }
+        if(target < 0 || target >= targetCount) return;
 
-    function animateSection(){
-        const moveY = -contentCount * window.innerHeight;
-        gsap.to(container,{
-            y : moveY,
-        })
+        contentCount = target;
+        console.log(contentCount);
+
+        gsap.to(window, {
+        duration: 1,
+        scrollTo: {
+            y: scrollTargets[target],
+            autoKill: false
+        }
+    });
     }
+    
 
     // 4.5초(4500밀리초)후 인트로 애니메이션 화면 DISPLAY NONE(가리기)
-    function openBgDelete(){
-        openBg = document.querySelector('.opening-container');
-        setTimeout(()=>{
-            openBg.style.display = 'none';
-        }, 4500)
-    }
+    //function openBgDelete(){
+    //    openBg = document.querySelector('.opening-container');
+    //    setTimeout(()=>{
+    //        openBg.style.display = 'none';
+    //    }, 4500)
+    //}
 })
